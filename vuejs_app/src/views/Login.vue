0e4d7b339643eb554454edcabb38a0a5aa4d1121
blupblupdoux <sarah.melin19403@gmail.com>
2021-03-05 17:09:32 +0100

    login page done

--- vuejs_app/src/views/Login.vue
+++ vuejs_app/src/views/Login.vue
@@ -1,36 +1,42 @@
 <template>
-<v-main>
-  <v-row>
-    <v-col cols="6">
-        <p>an image</p>
-    </v-col>
+<v-main class="align-center">
+  <v-col cols="10" class="mx-auto">
+    <v-row>
+      <v-col cols="7" class="pr-15">
+          <v-img src="../assets/calculator.svg"></v-img>
+      </v-col>
 
-    <v-col cols="6">
-      <h1>Connexion</h1>
-      <v-form @submit.prevent="onSubmit" ref="form">
-          <v-text-field
-            v-model="credentials.email"
-            label="Email"
-            :rules="[rules.require, rules.email]"
-            required
-          >
-          </v-text-field>
+      <v-col cols="5" class="d-flex flex-column justify-center pl-16">
+        <div>
+          <h1 class="primary--text mb-5">Connexion</h1>
+        </div>
+        <div>
+          <v-form @submit.prevent="onSubmit" ref="form">
+              <v-text-field
+                v-model="credentials.email"
+                label="Email"
+                :rules="[rules.require, rules.email]"
+                required
+              >
+              </v-text-field>
 
-          <v-text-field
-            v-model="credentials.password"
-            label="Mot de passe"
-            :rules="[rules.require]"
-            required
-          >
-          </v-text-field>
+              <v-text-field
+                v-model="credentials.password"
+                label="Mot de passe"
+                :rules="[rules.require]"
+                required
+              >
+              </v-text-field>
 
-          <div class="d-flex">
-            <v-btn type="submit">Se connecter</v-btn>
-            <v-alert v-if="connectionError" type="error" max-width="400px" class="ml-3" dense>Echec de la connexion, veuillez recommencer.</v-alert>
-          </div>
-      </v-form>
-    </v-col>
-  </v-row>
+              <div class="d-flex">
+                <v-btn type="submit" class="primary white--text">Se connecter</v-btn>
+                <v-alert v-if="connectionError" type="error" max-width="400px" class="ml-3" dense>Echec de la connexion, veuillez recommencer.</v-alert>
+              </div>
+          </v-form>
+        </div>
+      </v-col>
+    </v-row>
+  </v-col>
 </v-main>
 </template>
 
